971dd90310ef1b0913eb372d08487be08ebc0993
LechuMamais <119885029+LechuMamais@users.noreply.github.com>
2024-06-19 15:17:39 +0200

    FEAT: deteleImageFromTour controller and route!

--- src/api/controllers/tours.js
+++ src/api/controllers/tours.js
@@ -3,8 +3,8 @@ const Tours = require("../models/tours");
 const getTours = async (req, res, next) => {
     try {
         const tours = await Tours.find().populate('images.imgObj');
-        tours.forEach(tour =>{
-            tour.images.sort((a,b) =>{a.order - b.order});
+        tours.forEach(tour => {
+            tour.images.sort((a, b) => { a.order - b.order });
         })
         res.status(200).json(tours)
     } catch (error) {
@@ -15,8 +15,8 @@ const getTours = async (req, res, next) => {
 const getTourById = async (req, res, next) => {
     try {
         const tour = await Tours.findById(req.params.id).populate('images.imgObj');
-        if(tour){
-            tour.images.sort((a,b) =>{a.order - b.order});
+        if (tour) {
+            tour.images.sort((a, b) => { a.order - b.order });
         }
         res.status(200).json(tour);
     } catch (error) {
@@ -35,33 +35,58 @@ const createTour = async (req, res, next) => {
 
 const updateTour = async (req, res) => {
     const { name, heading, description, longDescription, images } = req.body;
-    
+
     try {
-      const updatedTour = await Tours.findByIdAndUpdate(
-        req.params.id,
-        {
-          name,
-          heading,
-          description,
-          longDescription,
-          images: images.map(img => ({
-            order: img.order,
-            imgObj: img.imgObj
-          })),
-        },
-        { new: true }
-      );
-  
-      if (!updatedTour) {
-        return res.status(404).json({ message: "Tour no encontrado" });
-      }
-  
-      res.status(200).json(updatedTour);
+        const updatedTour = await Tours.findByIdAndUpdate(
+            req.params.id,
+            {
+                name,
+                heading,
+                description,
+                longDescription,
+                images: images.map(img => ({
+                    order: img.order,
+                    imgObj: img.imgObj
+                })),
+            },
+            { new: true }
+        );
+
+        if (!updatedTour) {
+            return res.status(404).json({ message: "Tour no encontrado" });
+        }
+
+        res.status(200).json(updatedTour);
     } catch (error) {
-      res.status(500).json({ message: "Error al actualizar el tour", error });
+        res.status(500).json({ message: "Error al actualizar el tour", error });
     }
-  };
-  
+};
+
+const deleteImageFromTour = async (req, res, next) => {
+    const {tour_id, image_id} = req.params;
+    console.log(tour_id, image_id);
+    try {
+        const tour = await Tours.findById(tour_id);
+        if (!tour) {
+            return res.status(404).json({ message: "Tour no encontrado" });
+        }
+        console.log(tour)
+        let newImagesArray = [];
+        tour.images.forEach(image=>{
+            if(image.imgObj != image_id){
+                newImagesArray.push(image)
+            }
+        })
+        console.log(newImagesArray)
+        tour.images = [];
+        tour.images = newImagesArray;
+
+        const updatedTour = await Tours.findByIdAndUpdate(tour_id, tour, {new: true})
+        res.status(200).json(updatedTour);
+    } catch(error){
+        res.status(500).json({ message: "Error al actualizar el tour", error });
+    }
+}
 
 const deleteTour = async (req, res, next) => {
     try {
@@ -72,4 +97,4 @@ const deleteTour = async (req, res, next) => {
     };
 };
 
-module.exports = { getTours, getTourById, createTour, updateTour, deleteTour };
+module.exports = { getTours, getTourById, createTour, updateTour, deleteImageFromTour, deleteTour };
--- src/api/routes/tours.js
+++ src/api/routes/tours.js
@@ -1,12 +1,15 @@
 const { isAuthAdmin } = require("../../middlewares/isAuthAdmin");
 const { isAuth } = require("../../middlewares/isAuth");
-const { createTour, getTourById, getTours, updateTour, deleteTour } = require("../controllers/tours");
+const { createTour, getTourById, getTours, updateTour, deleteTour, deleteImageFromTour } = require("../controllers/tours");
 
 const toursRouter = require("express").Router();
 
 toursRouter.get("/:id", getTourById);
 toursRouter.get("/", getTours);
 toursRouter.post("/", isAuthAdmin, createTour);
+
+toursRouter.put("/:tour_id/deleteImage/:image_id", isAuthAdmin, deleteImageFromTour);
+
 toursRouter.put("/:id", isAuthAdmin, updateTour);
 toursRouter.delete("/:id", isAuthAdmin, deleteTour);
 
