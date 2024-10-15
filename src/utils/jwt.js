1a9f5a685b7a9eacce8d6a618a324d3f28b40fb9
LechuMamais <119885029+LechuMamais@users.noreply.github.com>
2024-10-15 12:57:54 +0200

    UPDATE: Token last for a year now!

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,13 +1,13 @@
 const jwt = require('jsonwebtoken');
 
-const generateKey = (id)=>{
-    return jwt.sign({id}, process.env.JWT_SECRET, {
-        expiresIn: 86400 // que sonn 24 horas
+const generateKey = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: 31536000
     });
 }
 
-const verifyKey = (token)=>{
+const verifyKey = (token) => {
     return jwt.verify(token, process.env.JWT_SECRET)
 }
 
-module.exports = { generateKey, verifyKey};
+module.exports = { generateKey, verifyKey };
