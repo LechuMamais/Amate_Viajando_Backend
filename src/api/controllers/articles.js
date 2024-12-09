884e9bea3217ed77584adf2b5fc93d0620cfa88b
LechuMamais <119885029+LechuMamais@users.noreply.github.com>
2024-12-09 23:52:54 +0100

    UPDATE: getArticles returns all images

--- src/api/controllers/articles.js
+++ src/api/controllers/articles.js
@@ -13,16 +13,16 @@ const getArticles = async (req, res, next) => {
             path: 'images.imgObj', // Poblamos el campo imgObj
             //options: { limit: 1 }, // Solo queremos el primer objeto del array
         });
-
-        const transformedArticles = articles.map(article => ({
-            _id: article._id,
-            title: article.title,
-            subtitle: article.subtitle,
-            images: [article.images[0]], // Incluimos solo el primer objeto, o null si no hay datos
-        }));
+        /*
+                const transformedArticles = articles.map(article => ({
+                    _id: article._id,
+                    title: article.title,
+                    subtitle: article.subtitle,
+                    images: [article.images[0]], // Incluimos solo el primer objeto, o null si no hay datos
+                }));*/
 
         // Enviar respuesta al cliente
-        res.status(200).json(transformedArticles);
+        res.status(200).json(articles);
     } catch (error) {
         console.error('Error al obtener los artículos:', error);
         res.status(404).json({ message: 'No se pudieron obtener los artículos', error });
